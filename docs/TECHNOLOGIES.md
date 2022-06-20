53f647ecb3ab7278fcb52445dbb2be702a07d5fc
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-20 21:54:52 +0300

    Update - Heroku Deployment

--- docs/TECHNOLOGIES.md
+++ docs/TECHNOLOGIES.md
@@ -1,14 +1,42 @@
-# [**Go Back**](../README.md)
+# [**Go Back**](https://github.com/lukebinmore/Guild-Of-Geeks)
 
 # **Table Of Contents**
 - [**Go Back**](#go-back)
 - [**Table Of Contents**](#table-of-contents)
 - [**Technologies Used**](#technologies-used)
   - [**Languages / Libraries / Frameworks**](#languages--libraries--frameworks)
+  - [**Applications**](#applications)
   - [**Services**](#services)
+  - [**Packages**](#packages)
 
 # **Technologies Used**
 
 ## **Languages / Libraries / Frameworks**
 
+- [Django - Base Python Framework](https://www.djangoproject.com/)
+
+***
+
+## **Applications**
+
+- [Balsamiq Wireframes - Wireframe Creation Tool](https://balsamiq.com/)
+- [Visual Studio Code - Development IDE](https://code.visualstudio.com/)
+
+***
+
 ## **Services**
+
+- [Heroku - Deployment](https://dashboard.heroku.com/apps)
+- [Cloudinary - Media Storage Service](https://cloudinary.com/)
+- [Canva - Logo Creation Tool](https://www.canva.com/)
+- [Google Fonts - Modern Fonts](https://fonts.google.com/)
+
+***
+
+## **Packages**
+
+- [dj_database_url - DB URL Environement Variable Formatter](https://pypi.org/project/dj-database-url/)
+- [psycopg2 - PostgreSQL Python Database Adapter](https://pypi.org/project/psycopg2/)
+- [gunicorn - Python WSGI HTTP Server](https://gunicorn.org/)
+
+***
--- guildofgeeks/settings.py
+++ guildofgeeks/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
+
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,9 +29,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = ['localhost']
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -40,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'forum',
 ]
 
@@ -78,12 +85,18 @@ WSGI_APPLICATION = 'guildofgeeks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
@@ -123,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
