6909f214bc03f465041115c5bf5b8146f0531f4c
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 18:31:02 +0300

    Update - Incorporated Summernote

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+all_models = [
+    models.Post,
+    models.Profile,
+    models.Category,
+    models.Tag,
+    models.Comment
+]
+
+
+class SummernoteAdmin(SummernoteModelAdmin):
+    summernote_fields = ('content')
+
+admin.site.register(all_models, SummernoteAdmin)
