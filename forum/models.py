bad64c3bf4ef8ce59eb8f2aa7f02fa60e6da5b34
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 15:58:41 +0300

    Update - Added Category Model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,13 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+
