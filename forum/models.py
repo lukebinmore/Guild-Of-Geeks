4fc21b7fc29aa2efacfc5964359712e1333cc18b
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 15:59:46 +0300

    Update - Added Post Model

--- forum/models.py
+++ forum/models.py
@@ -1,5 +1,9 @@
 from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+STATE = ((0, "Draft"), (1, "Post"))
 
 class Category(models.Model):
     title = models.CharField(max_length=50, unique=True)
@@ -20,3 +24,25 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    banner_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATE, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='blog_categories')
+    tags = models.ManyToManyField(Tag, on_delete=models.CASCADE, related_name='blog_tags')
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def likes_count(self):
+        return self.likes.count()
