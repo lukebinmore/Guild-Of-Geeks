bbbafdebee47311cc083e2e81c870b41399c2c75
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 15:59:10 +0300

    Update - Added Tag Model

--- forum/models.py
+++ forum/models.py
@@ -11,3 +11,12 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
