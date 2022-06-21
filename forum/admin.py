48bac23b9ad319e81a4f8f8423e0c3023c9d6fc5
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 20:10:07 +0300

    Update - Added Admin Panel Registrations

--- forum/admin.py
+++ forum/admin.py
@@ -2,16 +2,35 @@ from django.contrib import admin
 from . import models
 from django_summernote.admin import SummernoteModelAdmin
 
-all_models = [
-    models.Post,
-    models.Profile,
-    models.Category,
-    models.Tag,
-    models.Comment
-]
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'dob', 'dark_mode', 'created_on', 'updated_on',)
+    list_filter = ('dob', 'dark_mode', 'created_on', 'updated_on', 'followed_posts', 'followed_categories')
+    search_fields = ('user', 'first_name', 'last_name', 'dob', 'email', 'number', 'created_on', 'updated_on', 'followed_posts', 'followed_categories',)
 
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on',)
+    list_filter = ('created_on',)
+    search_fields = ('title',)
 
-class SummernoteAdmin(SummernoteModelAdmin):
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ('title',)
+
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_on', 'updated_on', 'status', 'category',)
+    list_filter = ('author', 'created_on', 'updated_on', 'status', 'category', 'tags',)
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'author', 'created_on', 'updated_on', 'status', 'category', 'tags',)
     summernote_fields = ('content')
 
-admin.site.register(all_models, SummernoteAdmin)
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_on', 'reply_to',)
+    list_filter = ('author', 'post', 'created_on', 'reply_to',)
+    search_fields = ('author', 'post', 'created_on', 'reply_to',)
+    summernote_fields = ('content')
