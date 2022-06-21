878656b307665ddcbabee1cdb8c06850543d46a9
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-21 17:01:04 +0300

    Bug Fix - Incorrect Argument in Post Model
    Issue:
    Incorrectly used on_delete argument in ManyToManyField for tags in Post Model.
    
    Resolution:
    Removed on_delete argument.

--- forum/models.py
+++ forum/models.py
@@ -30,21 +30,15 @@ class Tag(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     banner_image = CloudinaryField("image", default="placeholder")
     status = models.IntegerField(choices=STATE, default=0)
     likes = models.ManyToManyField(User, related_name="blog_likes", blank=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="blog_categories"
-    )
-    tags = models.ManyToManyField(
-        Tag, on_delete=models.CASCADE, related_name="blog_tags"
-    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="blog_categories")
+    tags = models.ManyToManyField(Tag, related_name="blog_tags")
 
     class Meta:
         ordering = ["-created_on"]
@@ -57,18 +51,12 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    reply_to = models.ForeignKey(
-        "self", on_delete=models.CASCADE, blank=True, null=True
-    )
-    likes = models.ManyToManyField(
-        User, related_name="comment_likes", blank=True
-    )
+    reply_to = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    likes = models.ManyToManyField(User, related_name="comment_likes", blank=True)
 
     class Meta:
         ordering = ["created_on"]
