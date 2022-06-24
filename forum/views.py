7770e0bd512104ad19d06acab09c4db91c0c7102
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-24 21:43:14 +0300

    Update - Replaced PostList view with updated Index View

--- forum/views.py
+++ forum/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.views import generic, View
 from . import models
 
-class PostList(generic.ListView):
+class Index(generic.ListView):
     model = models.Post
     queryset = models.Post.objects.filter(status=1).order_by('-created_on')
-
-class Index(PostList):
     template_name = 'forum/index.html'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super(Index, self).get_context_data(**kwargs)
+        context['categories'] = models.Category.objects.all()
+        context['tags'] = models.Tag.objects.all()
+        return context
