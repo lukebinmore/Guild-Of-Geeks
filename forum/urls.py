fdb914836c8458556e9d510179735c1c48de0491
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-06-29 05:32:30 +0300

    Update - Added URLs for Posting/Editing Posts

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('<slug:slug>/', views.Post.as_view(), name='post')
+    path('<slug:slug>/', views.PostView.as_view(), name='post-view'),
+    path('edit/<slug:slug>/', views.PostEdit.as_view(), name='post-edit')
 ]
