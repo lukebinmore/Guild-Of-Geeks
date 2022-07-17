12e6155076319310f272ba8ab17d47a4848aab5e
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-07-17 06:06:29 +0300

    Update - Profile Page Responsive Design

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,14 @@
 let searchForm = $('#search-form');
-
+let profileColLeft = $('#profile-col-left');
 let indexColLeft = $('#index-col-left');
 
 $(window).on('load resize', function () {
     if ($(window).width() < 768) {
         searchForm.addClass('input-group-sm');
+        profileColLeft.removeClass('pe-0');
     } else if ($(window).width() > 768) {
         searchForm.removeClass('input-group-sm');
+        profileColLeft.addClass('pe-0');
     };
 });
 
