46a34047262ff1c16f8f9ef0a3ba79cfab60109d
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-07-24 02:12:11 +0300

    Update - Added Redirect Page Function for AJAX

--- forum/functions.py
+++ forum/functions.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 def previous_page(request):
     return render(
@@ -12,4 +13,12 @@ def previous_page(request):
 def form_field_errors(form):
     for field in form:
         if field.errors:
-            return f'{field.name.title()} : {field.errors[0]}'
+            return f'{field.name.title()} : {field.errors[0]}'    return render(
+        request,
+        'redirect.html',
+        {
+            'redirect_url': reverse(page, kwargs=kwargs)
+        }
+    )
+
+def form_field_errors(*args):
