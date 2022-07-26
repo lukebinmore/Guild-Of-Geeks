1136008e0805c5f839056264f348ac6ee06b119a
Luke Binmore <93396827+lukebinmore@users.noreply.github.com>
2022-07-26 18:24:22 +0300

    Update - Added Author Check Decorators

--- forum/decorators.py
+++ forum/decorators.py
@@ -1,6 +1,7 @@
 from . import functions as f
 from functools import wraps
 from django.contrib import messages
+from . import models
 
 def login_required(function):
     @wraps(function)
@@ -10,4 +11,28 @@ def login_required(function):
         else:
             messages.error(request, 'You must be logged into view this page!')
             return f.redirect_page(request, 'index')
+    return wrap
+
+def check_post_author(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        slug = kwargs['slug']
+        post = f.get_object(models.Post, slug=slug)
+        if post.author == request.user or request.user.is_staff:
+            return function(request, *args, **kwargs)
+        
+        messages.error(request, 'This item does not belong to you!')
+        return f.redirect_page(request, 'index')
+    return wrap
+
+def check_comment_author(function):
+    @wraps(function)
+    def wrap(request, *args, **kwargs):
+        id = kwargs['id']
+        comment = f.get_object(models.Comment, id=id)
+        if comment.author == request.user or request.user.is_staff:
+            return function(request, *args, **kwargs)
+        
+        messages.error(request, 'This item does not belong to you!')
+        return f.redirect_page(request, 'index')
     return wrap
